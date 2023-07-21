2cc2e3eaaa62a818ee6a82a776e9374f71584bb2
hyeon9782 <hyeon9782@gmail.com>
2023-07-22 00:09:00 +0900

    feat: plan crud api 개발

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("dotenv").config(); // 모듈 불러오기
 const plans = [
   {
     userId: "0",
+    planId: "1",
     name: "일본 여행",
     city: ["도쿄", "오사카"],
     startDate: "2023.7.12",
@@ -32,6 +33,7 @@ const plans = [
   },
   {
     userId: "0",
+    planId: "2",
     name: "중국 여행",
     city: ["베이징", "텐진", "청두"],
     startDate: "2023.8.12",
@@ -62,14 +64,40 @@ app.get("/api/plan/:userId", async (req, res) => {
   res.json(plans.filter((plan) => plan.userId === req.params.userId));
 });
 
-// // 특정 여행 계획을 조회하는 API
-// app.get("/api/plan/:planId", async (req, res) => {});
+// 특정 여행 계획을 조회하는 API
+app.get("/api/plan/:planId", async (req, res) => {
+  console.log(req.params);
+  console.log(req.params.planId);
+  res.json(plans.find((plan) => plan.planId === req.params.planId));
+});
 
-app.post("/api/plan", async (req, res) => {});
+// 여행 계획을 저장하는 API
+app.post("/api/plan", async (req, res) => {
+  const newPlan = req.body;
+  plans.push(newPlan);
+  res.status(201).json(newPlan);
+});
 
-app.put("/api/plan/:planId", async (req, res) => {});
+// 여행 계획을 수정하는 API
+app.put("/api/plan/:planId", async (req, res) => {
+  const { planId } = req.params;
+  const updatedPlan = req.body;
+  const index = plans.findIndex((plan) => plan.planId === planId);
 
-app.delete("/api/plan/:planId", async (req, res) => {});
+  if (index === -1) {
+    res.status(404).json({ message: "여행 계획이 없습니다." });
+  } else {
+    plans[index] = { ...updatedPlan, planId };
+    res.json(plans[index]);
+  }
+});
+
+// 여행 계획을 삭제하는 API
+app.delete("/api/plan/:planId", async (req, res) => {
+  const { planId } = req.params;
+  plans = plans.filter((plan) => plan.planId !== planId);
+  res.sendStatus(204);
+});
 
 app.get("/api/search", async (req, res) => {
   const { keyword, latitude, longitude, radius } = req.query;
